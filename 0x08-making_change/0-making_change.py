ec23eabb814cb4dcb0db4a635912398cc94aebb8
Opiro1 <johnsonopeoluwa2304@gmail.com>
2023-08-26 13:14:26 -0700

    The making change project

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Change comes from within"""
+"""Making Change Problem"""
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values,
-    determining fewest num of coins needed to meet a given amount total"""
+    """Determines the fewest number of coins needed
+        to meet a given amount total"""
     if total <= 0:
         return 0
-    pile = [0] + [float("inf")] * (total)
+
+    current_total = 0
+    used_coins = 0
+    coins = sorted(coins, reverse=True)
     for coin in coins:
-        for x in range(coin, total + 1):
-            pile[x] = min(pile[x], pile[x - coin] + 1)
-    return pile[-1] if pile[-1] != float("inf") else -1
+        r = (total - current_total) // coin
+        current_total += r * coin
+        used_coins += r
+        if current_total == total:
+            return used_coins
+    return -1
